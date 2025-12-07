93666d5f73b2bb0ceb2066aaca9e05c2c043ee39
acrubaugh11 <abcrubaugh@uncg.edu>
2025-12-06 19:52:54 -0500

    sessions

--- server.js
+++ server.js
@@ -26,11 +26,12 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie:{
+      httpOnly: true,
       secure: true,
       sameSite: 'none',
-      maxAge: 24*60*60*1000
+      maxAge: 1000 * 60 * 60 * 24,
 
-    }
+    },
   })
 );
 

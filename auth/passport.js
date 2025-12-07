f9b8cb54aa683964c4aca54e38f7e134f0c9bebe
acrubaugh11 <abcrubaugh@uncg.edu>
2025-12-06 20:07:34 -0500

    session update

--- auth/passport.js
+++ auth/passport.js
@@ -26,7 +26,7 @@ passport.use(new GoogleStrategy({
 
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user.googleId);
 });
 
 passport.deserializeUser(async (id, done) => {

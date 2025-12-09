70e4152531956bfa4c9ef7ce5e169fdb4f6ca19b
acrubaugh11 <abcrubaugh@uncg.edu>
2025-12-09 16:09:36 -0500

    secret key insteead of client secret

--- auth/passport.js
+++ auth/passport.js
@@ -5,7 +5,7 @@ const userModel = require("../models/userModel");
 
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
+  clientSecret: process.env.SECRET_KEY,
   callbackURL: `${process.env.VITE_BACKEND_API_BASE_URL}/auth/google/callback`,
   passReqToCallback: true
 }, async (req, accessToken, refreshToken, profile, done) => {

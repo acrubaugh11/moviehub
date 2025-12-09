9f0371334b5a00e06eceac5fd5a908c48bc540ce
acrubaugh11 <abcrubaugh@uncg.edu>
2025-12-09 15:51:15 -0500

    process.env.port

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/{*splat}', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, function () {
   console.log("Server listening on port: " + PORT + "!");
 });
